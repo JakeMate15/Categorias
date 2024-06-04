4e0dd3da102ca43da230ac2e8a0acb77a36e468b
JakeMate15 <erik.mofe51@gmail.com>
2024-06-03 19:42:17 -0600

    Calculadora Actividades

--- AppCategorias/src/app/actividades/actividades.component.ts
+++ AppCategorias/src/app/actividades/actividades.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-actividades',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './actividades.component.html',
   styleUrl: './actividades.component.css'
 })
 export class ActividadesComponent {
+    titulo: string = 'Operaciones Basicas';
+    n1: number = 0;
+    n2: number = 0;
+    res: number = 0;
 
+    sumar(): void {
+        this.res = this.n1 + this.n2;
+    }
+
+    resta(): void {
+        this.res = this.n1 - this.n2;
+    }
+
+    multiplicacion(): void {
+        this.res = this.n1 * this.n2;
+    }
+
+    division(): void {
+        this.res = this.n1 / this.n2;
+    }
 }
--- AppCategorias/src/app/app.routes.ts
+++ AppCategorias/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ActividadesComponent } from './actividades/actividades.component';
 
 export const routes: Routes = [
     {path: '', redirectTo:'/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'actividades', component:ActividadesComponent}
 ];
